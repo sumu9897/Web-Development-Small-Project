25ae802ade4c96601ed7427084605d98ae97c047
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2023-12-16 11:08:05 +0600

    [Done] Restaurant Website

---
+++ Restaurant Website/script.js
@@ -0,0 +1,10 @@
+function makeReservation() {
+    var name = document.getElementById('name').value;
+    var date = document.getElementById('date').value;
+    var time = document.getElementById('time').value;
+    var guests = document.getElementById('guests').value;
+
+    // Implement reservation logic here
+
+    alert('Reservation functionality not implemented in this example.');
+}
